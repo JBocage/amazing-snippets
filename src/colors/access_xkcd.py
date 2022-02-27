34319643ee0a15e21992015b2bab3a94bd2f14da
Julien Bocage <julien.bocage@gmail.com>
2022-02-27 15:19:59 +0100

    Colorful snippets

---
+++ src/colors/access_xkcd.py
@@ -0,0 +1,7 @@
+"""
+This snippet provides a function that can be used to get codes of a huge colors dataset
+"""
+
+def color(colorname):
+    import matplotlib.colors as mcl
+    return mcl.XKCD_COLORS['xkcd:'+colorname]
